367ea7289eb8755b3ef7e5e64234bc58e74ad4b8
fuling <749864961@qq.com>
2017-11-01 10:16:05 +0800

    添加公共样式

--- v1.0/web/src/router/index.js
+++ v1.0/web/src/router/index.js
@@ -1,68 +1,3 @@
-// <<<<<<< HEAD
-// import Vue from 'vue'
-// import VueRouter from 'vue-router'
-// import Element from 'element-ui'
-
-// import LoginComponent from '../components/login/login.vue'
-// import HomeComponent from '../components/home/home.vue'
-// import FoodsListComponent from '../components/foodslist/foodslist.vue'
-// import FoodsFormComponent from '../components/foodsform/foodsform.vue'
-// import ListComponent from '../components/list/list.vue'
-// import BackstageComponent from '../components/backstage/backstage.vue'
-// import Backstage_OrdFood from '../components/backstage_Ord/backstage_Ord.vue'
-// import Backstage_AddFood from '../components/backstage_Add/backstage_Add.vue'
-// import Backstage_AdmFood from '../components/backstage_Adm/backstage_Adm.vue'
-
-// Vue.use(VueRouter)
-
-// var router = new VueRouter({
-// 	routes: [
-// 		{
-// 			path: '/',
-// 			name: 'home',
-// 			component: HomeComponent,
-// 			children: [{
-// 				path: '/foodslist',
-// 				name: 'foodslist',
-// 				component: FoodsListComponent
-// 			},{
-// 				path: '/foodsform',
-// 				name: 'foodsform',
-// 				component: FoodsFormComponent
-// 			}]
-// 		},
-// 		{
-// 			path: '/login',
-// 			name: 'login',
-// 			component: LoginComponent
-// 		},
-// 		{
-// 			path:'/list',
-// 			name:'list',
-// 			component:ListComponent
-// 		},
-// 		{
-// 			path:'/backstage',
-// 			name:'backstage',
-// 			component:BackstageComponent,
-// 			children: [{
-// 				path: '/OrdFood',
-// 				name: 'OrdFood',
-// 				component:Backstage_OrdFood
-// 			},{
-// 				path: '/AddFood',
-// 				name: 'AddFood',
-// 				component:Backstage_AddFood
-// 			},{
-// 				path: '/AdmFood',
-// 				name: 'AdmFood',
-// 				component:Backstage_AdmFood
-// 			}
-// 			]
-// 		}
-// 	]
-// })
-
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import LoginComponent from '../components/login/login.vue'
@@ -142,8 +77,7 @@ var router = new VueRouter({
 				path: '/AdmFood',
 				name: 'AdmFood',
 				component:Backstage_AdmFood
-			}
-			]
+			}]
 		}
 	]
 })
